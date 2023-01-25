33ae73bb9fe916b9239924e9e6440408a90a61d0
Elliott <elliott.ventura26@gmail.com>
2023-01-25 15:39:27 -0600

    finished section 1 and 2

---
+++ script.js
@@ -0,0 +1,24 @@
+// Section 1.
+
+// let a = 0; ---- valid
+// const b = 1; ---- valid
+// 9 = a; ---- nope
+// b = b + 5; ---- valid
+
+// Section 2.
+
+// const a = 42.78
+// const b = 1.1
+// const c = -2
+// const d = .5
+// const e = 16
+
+// console.log(Math.floor(a)) 
+// console.log(Math.ceil(b))
+// console.log(Math.abs(b - a))
+// console.log(Math.max(a, b, c, d, e))
+// console.log(Math.min(a, b, c, d, e))
+// console.log(Math.sqrt(e))
+// console.log(Math.pow(e, d))
+
+ 
