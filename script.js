8060f29f8f8abe433c912f408cab5f1afc29fdff
narniaeagle <ilgar2023@hotmail.com>
2023-01-25 18:11:33 -0500

    all done I hope

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ console.log("Hello, my name is " + yourName)
 // a == a != d;
 // e == 'Jelly Bean';
 // 'e' != 'Eh'
-
+//5
 let animal = "cow"
 if(animal=="cow"){
     console.log("mooooo")
@@ -56,3 +56,90 @@ else{
     console.log("Hey! You're not a cow.")
 }
 
+//6
+let prsnAge = 13
+if(prsnAge >= 16){
+    console.log("Here are the keys")
+}
+else{
+    console.log("Sorry, you're too young.")
+}
+
+//7
+for(let i =0; i<11; i++){
+    console.log(i)
+}
+for(let i=500; i>=50; i--){
+    console.log(i)
+}
+
+//8
+for(let i=1; i<=100; i++){
+    if(i%2 != 0){
+        console.log(`${i} is an odd number`)
+    }
+}
+
+//9
+for(let i=0; i<=100; i++){
+    if(i % 5 == 0){
+        console.log(`I found a ${i}. High five!`) // Included 0 too
+    }
+    if(i % 3 == 0){
+        console.log(`I found a ${i}. Three is a crowd`)
+    }
+}
+
+//10
+let bank1 = 0
+let bank2 = 0
+for(let i =1; i<=10; i++){
+    bank1 += i
+}
+console.log(bank1)
+for(let i =1; i<=100; i++){
+    bank2 += i*2
+}
+console.log(bank2)
+
+//11
+for(i=1; i<=100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz")
+    }
+    else if(i % 3 == 0){
+        console.log("Fizz")
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz")
+    }
+    else{
+        console.log(i)
+    }
+}
+
+//12
+let num1 = 0
+let num2 = 1
+let hold
+for (i=0; i<=10; i++){
+    console.log(num1, num2)
+    hold = num2
+    num2 = num1 + num2
+    num1 = hold
+}
+
+//13
+const boardSize = 20
+
+width = Math.floor(boardSize/2)
+for(i=0; i<=boardSize; i++){
+    let pattern = "# "
+    if(i % 2 != 0){
+        console.log(pattern.repeat(width))
+    }
+    else{
+        console.log(" "+pattern.repeat(width))
+    }
+}
+console.log(width)
