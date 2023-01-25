44b70ce0e1e1623410156e1d3863b6173298274a
Elliott <elliott.ventura26@gmail.com>
2023-01-25 16:28:01 -0600

    completed up to section 7

--- script.js
+++ script.js
@@ -21,4 +21,59 @@
 // console.log(Math.sqrt(e))
 // console.log(Math.pow(e, d))
 
- 
+ // Section 3.
+
+//  let firstVariable = `Hello World`
+//  firstVariable = 25
+//  let secondVariable = firstVariable
+//  secondVariable =  `Goodbye`
+// firstVariable is still going to be 25
+// let yourName = "Elliott"
+// console.log(`Hello, my name is ${yourName}!`)
+
+// const a = 6;
+// const b = 100;
+// const c = -5;
+// const d = 3000;
+// const e = 'Jelly Bean';
+
+// console.log(a < b)
+// console.log(c < d)
+// console.log('Peanut' == 'Peanut')
+// console.log(a < b > c)
+// console.log(a <= a < d)
+// console.log(e == 'Jelly Bean')
+// console.log(48 == '48')
+//console.log('e' !== 'Eh') 
+
+// Section 5.
+
+// let animal = 'cow'
+
+// if (animal == 'cow')
+//     { console.log(`Mooooo`)
+// } else {
+//     console.log(`You ain't no cow!`)
+// }
+
+// Section 6.
+
+// let age = 13
+
+// if (age >= 16) {
+// console.log(`Here are the keys`)
+// } else{
+//     console.log(`Nah, you're too young`)
+// }
+
+
+// Section 7.
+
+// for (let i = 0; i < 11; i++){
+// console.log([i])
+// }
+
+for (let i = 500; i > 49; i--){
+  console.log([i])
+}
+
