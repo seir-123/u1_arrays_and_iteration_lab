9e162b093236fa6b4d2f1760d7de5f1b1b2da219
narniaeagle <ilgar2023@hotmail.com>
2023-01-25 16:55:56 -0500

    chapter 6

---
+++ script.js
@@ -0,0 +1,58 @@
+//How do we assign a value to a variable?
+//single equal sign value is on the right and variable is on the left
+//let a = 0; valid
+//const b = 1; valid
+//9 = a;  not valid
+//b = b + 5; valid b= 1+5 6
+//How do we change the value of a variable?
+//name of the variable = value
+//How do we assign an existing variable to a new variable?
+//new variable = existing variable
+
+const a = 42.78
+const b = 1.1
+const c = -2
+const d = .5
+const e = 16
+
+Math.floor(a)
+Math.ceil(b)
+Math.abs(b, a)
+Math.max(a, b, c, d, e)
+Math.min(a, b, c, d, e)
+Math.sqrt(e)
+Math.pow(e, d)
+Math.floor(Math.random()*6)+1
+
+let firstVariable
+firstVariable = "Hello World"
+firstVariable = 6
+let secondVariable = firstVariable
+secondVariable = "Hi"
+//the number that I assigned which is 6
+
+let yourName = "Alpagu"
+console.log("Hello, my name is " + yourName)
+
+// const a = 6;
+// const b = 100;
+// const c = -5;
+// const d = 3000;
+// const e = 'Jelly Bean';
+
+// a < b
+// c < d
+// 'Peanut' == 'Peanut';
+// a < b != c;
+// a == a != d;
+// e == 'Jelly Bean';
+// 'e' != 'Eh'
+
+let animal = "cow"
+if(animal=="cow"){
+    console.log("mooooo")
+}
+else{
+    console.log("Hey! You're not a cow.")
+}
+
