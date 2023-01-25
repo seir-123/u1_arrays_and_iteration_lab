6d0725c917f02958e6dab87a2a3db683584a2b23
Toni <windtronic89@gmail.com>
2023-01-25 15:07:44 -0700

    rough draft

---
+++ index.js
@@ -0,0 +1,36 @@
+//1) How do we assign a value to a variable?
+//Which line(s) is/are valid?
+
+// let b = 2;
+// let a = 6;
+// console.log(a)
+
+//2) How do we change the value of a variable?
+
+// let a = 3;
+// console.log(a)
+
+//3) How do we assign an existing variable to a new variable?
+
+//Section 2. Let's get Mathy!
+const a = 42.78
+const b = 1.1
+// const c = -2
+// const d = .5
+// const e = 16
+
+// Round a down 
+console.log(Math.floor(42.78))
+// Round b up
+console.log(Math.ceil(1.1))
+// Find the absolute value of b - a
+console.log(Math.abs(b-a))
+// Find the largest number of a, b, c, d, e
+
+// Find the smallest number of a, b, c, d, e
+
+// Find the square root of e
+
+// Raise e to the power of d
+
+// Make a digital die 0 generate a random number between 1 - 6
