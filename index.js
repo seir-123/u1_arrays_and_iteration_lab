d6716d2fb14ace84775c8a24f4e32d1a65c06b93
Toni <windtronic89@gmail.com>
2023-01-26 07:14:11 -0700

    ready to submit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //1) How do we assign a value to a variable?
 //Which line(s) is/are valid?
 
@@ -11,26 +12,111 @@
 // console.log(a)
 
 //3) How do we assign an existing variable to a new variable?
-
+// let a = 2;
 //Section 2. Let's get Mathy!
-const a = 42.78
-const b = 1.1
+// const a = 42.78
+// const b = 1.1
 // const c = -2
 // const d = .5
 // const e = 16
 
-// Round a down 
-console.log(Math.floor(42.78))
-// Round b up
-console.log(Math.ceil(1.1))
-// Find the absolute value of b - a
-console.log(Math.abs(b-a))
+// // Round a down 
+// console.log(Math.floor(42.78))
+
+// // // Round b up
+// console.log(Math.ceil(1.1))
+
+// // Find the absolute value of b - a
+// number = Math.abs(b - a)
+// console.log(number)
+
 // Find the largest number of a, b, c, d, e
+// console.log(Math.max(42.78, 1.1, -2, .5, 16))
 
 // Find the smallest number of a, b, c, d, e
+//console.log(Math.min(42.78, 1.1, -2, .5, 16))
 
 // Find the square root of e
+//console.log(Math.sqrt(e))
 
 // Raise e to the power of d
+// let power = Math.pow(e, d);
+// console.log(power)
 
 // Make a digital die 0 generate a random number between 1 - 6
+// console.log(Math.random() * 6)
+
+
+
+//Section 3 Strings - Switcharoo
+
+// Create a variable called firstVariable.
+// // assign it the value of a string: "Hello World"
+// let firstVariable = "Hello World"
+
+// // On the next line, change the value of this variable to a number.
+// let firstVariable = 5
+
+// // store the value of firstVariable into a new variable called secondVariable
+// let secondVariable = firstVariable
+
+// On the next line, change the value of secondVariable to a string.
+
+// What is the value of firstVariable?
+// Strings - Combine it
+
+// Create a variable called yourName and set it equal to your name as a string.
+// let myName = "Toni"
+
+// // Write an expression that takes the string "Hello, my name is " and the variable yourName so that it prints a new string with them concatenated
+// console.log("Hello, my name is" + " " + myName)
+// / ex: Hello, my name is Jean Valjean
+
+
+
+
+// Section 6. Driver's Ed
+
+// Write a variable that will hold a person's age.
+// let age = 15
+// // Write code that will print out "Here are the keys", if the age is 16 years or older.
+// if(age>16) {
+//     console.log("Here are the keys")
+// }
+// // Change your code so that if the age is younger than 16, a message will print "Sorry, you're too young."
+//  else {
+//     console.log("Sorry, you're too young.")
+//  }
+
+
+
+
+
+//  Section 7. Just Loop It
+
+// Write code that will print out all the numbers in the range 0 - 10.
+// for (let i = 0; i < 11; i++) {
+// console.log(i)
+// }
+// // Write code that will print out all the numbers in the range 500 - 50.
+// for (let i = 50; i < 501; i++) {
+//     console.log(i)
+// }
+
+
+
+
+// Section 8. Let's get odd
+
+// // Print out the odd numbers that are within the range of 1 - 100.
+// for (let i = 1; i < 100; i += 2) {
+// console.log(i)
+// }
+// }
+// Adjust your code to add a message next to the odd number that says: "is an odd number".
+
+// Example Output:
+
+// 3 is an odd number
+// 5 is an odd number
+// 7 is an odd number
