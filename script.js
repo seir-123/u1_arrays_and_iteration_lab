c8b16a5ebd21aa271c7047ac7a2522eda231951b
Steph <tephpez@gmail.com>
2023-01-26 08:34:40 -0500

    3.5/5 complete

---
+++ script.js
@@ -0,0 +1,71 @@
+// SECTION 2
+
+const a = 42.78
+const b = 1.1
+const c = -2
+const d = .5
+const e = 16
+
+
+console.log(Math.floor(a))
+
+console.log(Math.ceil(b))
+
+console.log(Math.abs(b - a))
+
+console.log(Math.max(a, b, c, d, e))
+
+console.log(Math.min(a, b, c, d, e))
+
+console.log(Math.sqrt(e))
+
+console.log(Math.pow(e, d))
+
+console.log(Math.floor(Math.random() * 6) + 1)
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// SECTION 3
+
+    //  SWITCHAROO
+
+        let firstVariable = (`Hello World`)
+        firstVariable = 5
+        // store the value of `firstVariable` into a new variable called `secondVariable`
+        secondVariable = ("Goodbye World")
+        // What is the value of `firstVariable`?
+
+        console.log(firstVariable)
+
+    //  COMBINE IT
+
+        let yourName = ("Steph Pez")
+
+        console.log(`Hello, my name is ${yourName}`)
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// SECTION 6
+
+let age = 12
+
+if (age > 16) {
+    console.log("Here are the keys")
+}
+else if (age < 16) {
+    console.log("Sorry, you're too young")
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// SECTION 7
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+////////////////////////////////////////////////////////////////////////////////////////
+
+// SECTION 8
