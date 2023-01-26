5f95a5285d13a44b4da7b158aa4710a525cf2dce
Charlotte Fusco <ch.fusco104@gmail.com>
2023-01-25 22:28:10 -0600

    Completed sections 2, 3, 6, 7, 8

---
+++ script.js
@@ -0,0 +1,54 @@
+//SECTION 2
+
+// const a = 42.78
+// const b = 1.1
+// const c = -2
+// const d = .5
+// const e = 16
+
+// console.log(Math.floor(a))
+// console.log(Math.ceil(b))
+// console.log(Math.abs(b-a))
+// console.log(Math.max(a, b, c, d, e))
+// console.log(Math.min(a, b, c, d, e))
+// console.log(Math.sqrt(e))
+// console.log(Math.pow(e, d))
+// console.log(Math.floor(Math.random() * 6) + 1) // Math.floor rounds numbers down to whole number, Math.random generates random number b/w 0-5 and adding +1 allows that to change to 1-6; Used this to get the answer: https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+
+
+//SECTION 3
+
+// let firstVariable = "Hello World"
+// firstVariable = 5
+// let secondVariable = firstVariable
+// secondVariable = "This is a string."
+// console.log(firstVariable) // The value is 5.
+
+// let yourName = "Charlotte Fusco"
+// console.log(`Hello, my name is ${yourName}.`)
+
+//SECTION 6
+
+// let age = 12
+
+// if (age >= 16) {
+//   console.log("Here are the keys.")
+// } else {
+//   console.log("Sorry, you're too young.")
+// }
+
+//SECTION 7
+
+// for (i = 0; i < 11; i++) {
+//   console.log(i)
+// }
+
+// for (i = 500; i > 49; i--) {
+//   console.log(i)
+// }
+
+//SECTION 8
+
+// for (i = 1; i < 100; i += 2) {
+//   console.log(`${i} is an odd number.`)
+// }
