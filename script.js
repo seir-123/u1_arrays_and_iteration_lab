355aca8256aec0360c01d3317a90a1a569e3466a
Reina Giraldo <ffcmalka@gmail.com>
2023-01-26 08:33:28 -0500

    Homework submission Wednesday

---
+++ script.js
@@ -0,0 +1,66 @@
+// Section 1
+// 1. line 1,2, and 4 
+// 2. by reassigning the value using =
+// 3. by reassigning the left variables over to the right variable
+
+// Section 2
+// 1. const a = 42.78  
+// 2. const b = 1.1
+// 3. const c = -2
+// 4. const d = .5
+// 5. const e = 16
+
+// 1- math.floor()
+// 2- math.ceil()
+// 3- 41.68
+// 4- a 
+// 5- c
+// 6- 4
+// 6- 4
+// 8- let die = math.floor(math.random()*6)+1;
+
+// Section 3
+
+// 1 & 2. let firstVariable = "Hello World"
+//         if (firstVariable) {
+//         console.log("Hello World")
+//   }
+
+
+// 3. let firstVariable = 22
+
+// 4. firstVariable = secondVariable
+
+// 5. let secondVariable = "Hello World"
+
+// 6. There is no value?
+
+// 7. let yourName = "Reina Giraldo"
+// if(yourName) {
+//   console.log("Reina Giralfo")
+// }
+
+// Section 6
+
+// let age = 21
+
+// if (age >= 16){
+//   console.log("Here are the keys")
+//   }
+//   else if(age < 16){
+//    console.log("Sory,you're too young.")
+//    }
+  
+// Section 7
+
+// 1. for (let i = 0; i <= 10; i++) {
+// console.log(i);
+// }
+
+// 2. for (let i = 500; i >= 50; i--) {
+//   console.log(i);
+// }
+
+
+// Section 8
+ 1. This section got me a bit stumped, so I would love some more practice with for loops
