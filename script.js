baf78a722433c4a3833a9cb52b5e6618f28c7cb1
Faatimah Cleveland <faatimahcleveland@gmail.com>
2023-01-26 23:44:23 -0500

    80 percent

---
+++ script.js
@@ -0,0 +1,138 @@
+// Section 1
+// #1
+// let a = 0; yes it's valid t,his line creates a new variable "a" and assigns it the value of 0.
+// const b = 1; valid - the const keyword is where a varibale is delared that does allow the value to be changed 
+// 9 = a; not valid it should be flipped "a" doe snot have value
+// b = b + 5;  not valid 
+// #2 
+// to change the value add "=" by using the varibale name on the left side and the new value on the right side
+// #3 too assgn an existing variable to a new variable use "="
+
+//  Section 2
+//  
+// const a = 42.78; 
+// const roundedA = Math.floor(a);
+// console.log(roundedA);
+// 
+// const b = 1.1;
+// const roundedUp = Math.ceil(b);
+// console.log(roundedUp);
+//
+// const a = 42.78;
+// const b = 1.1; 
+// console.log(Math.abs(b - a));
+// 
+// const a = 42.78
+// const b = 1.1
+// const c = -2
+// const d = .5
+// const e = 16
+// console.log(Math.max(a, b, c, d, e))
+// 
+// const a = 42.78
+// const b = 1.1
+// const c = -2
+// const d = .5
+// const e = 16
+// console.log(Math.min(a, b, c, d, e))
+//
+// const e = 16
+// console.log(Math.sqrt(e));
+//
+//const e = 16 
+//const d = .5
+//console.log(e ** d);          // short for math.pow
+//
+// const randomNumber = Math.floor(Math.random() * 6) + 1;
+// console.log(randomNumber);
+//
+//  Section 3 Strings
+//
+// let firstVariable = "Hello World"; 
+// let secondVariable = firstVariable;
+// secondVariable = "String"
+// console.log(firstVariable);
+// "Hello World"
+//
+//  Section 4 strings-combine it
+// let yourName = "Faatimah";
+// console.log(`Hello, my name is ${yourName}`);
+// 
+//      Section 5 
+//
+ //let animal = "pig";
+ //if (animal === "cow") {
+ //   console.log("moooo");
+ // } else {
+ //   console.log("Her! You're not a cow.")
+ // }
+ //
+//  Section 6
+//
+// let age = 15
+// if (age >= 16) {
+ //   console.log("Here are the keys")
+// } else {
+  //  console.log("sorry, youre too young")
+//  }
+//
+//  Section 7
+//
+//f or (let i = 0; i <= 10; i++) {
+ //   console.log(i)
+// }
+// for (let i = 500; i>= 50; i --) {
+//    console.log(i)
+//  }
+// 
+//  Section 8
+//
+//  for (let i = 1; i<=100; i++) {
+//    if (i % 2 !== 0) {
+//        console.log(i + "is an odd number");
+//    }
+//  }
+//
+// Section 9 
+//
+// for (let i = 0; i <= 100; i++) {
+ //   if (i % 5 === 0) {
+   //     console.log("I found a number. High five!")
+ //   } if (i % 3 === 0) { console.log("I found a number. Three is a crowd")}
+// }
+//
+// Section 10
+//
+// let bankAccount = 0;
+// for(let i = 1; i <=10; i++){
+ //   bankAccount +=i;
+// }
+//  console.log(bankAccount);
+//
+// let bankAccount = 0;
+// for(let i = 1; i<=100; i++){
+ //   bankAccount +=(i*2);}
+// console.log(bankAccount);
+// Section 11 
+// 
+//for (let i = 1; i <= 100; i++) {
+ //       if (i % 3 === 0) {
+ //           console.log ("Fizz");    
+//    } else {
+//        console.log(i);
+//    }
+//}
+// for (let i = 1; i <= 100; i++) {
+//    if (i % 5 === 0) {
+ //       console.log ("buzz");    
+//} else {
+ //   console.log(i);
+//}
+//}
+// for (let i = 1; i <= 100; i++) {
+ //   if (i % 3 === 0 && i % 5 === 0) {
+ //       console.log ("Fizzbuzz");    
+//} else {
+ //   console.log(i);
+//}
+//}
