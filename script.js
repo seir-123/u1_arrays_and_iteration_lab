78361abd3520aa28adefce75ebe34a91ffc9395f
Steph <tephpez@gmail.com>
2023-01-30 00:53:11 -0500

    node not working

--- script.js
+++ script.js
@@ -27,33 +27,37 @@ console.log(Math.floor(Math.random() * 6) + 1)
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // SECTION 3
+//  SWITCHAROO
 
-    //  SWITCHAROO
+    let firstVariable = (`Hello World`)
+    firstVariable = 5
+    let secondVariable = firstVariable
+    secondVariable = ("Goodbye World")
+    // What is the value of `firstVariable`?
 
-        let firstVariable = (`Hello World`)
-        firstVariable = 5
-        // store the value of `firstVariable` into a new variable called `secondVariable`
-        secondVariable = ("Goodbye World")
-        // What is the value of `firstVariable`?
+    console.log(firstVariable);
 
-        console.log(firstVariable)
 
-    //  COMBINE IT
+//  COMBINE IT
 
-        let yourName = ("Steph Pez")
+    let yourName = ("Steph Pez")
 
-        console.log(`Hello, my name is ${yourName}`)
+    console.log(`Hello, my name is ${yourName}`);
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // SECTION 6
-
+// 1) 
 let age = 12
 
+
+// 2)
 if (age > 16) {
     console.log("Here are the keys")
 }
+
+// 3)
 else if (age < 16) {
     console.log("Sorry, you're too young")
 }
@@ -62,10 +66,34 @@ else if (age < 16) {
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // SECTION 7
-
-for (let i = 0; i <= 10; i++) {
+// 1)
+for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+// 2)
+for (let i = 50; i <= 500; i++) {
+    console.log(i);
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // SECTION 8
+// 1)
+let x = 1
+let y = 2
+
+for (let i = x ; i < 100 ; i += 2 ) {
+    console.log(i);
+}
+
+for (let i = y ; i < 100 ; i += 2 ) {
+    console.log(i);
+}
+
+// sourced code from stackoverflow & edited... does this work? it seemed excessive to list all numbers from 1-100...
+// https://stackoverflow.com/questions/66837818/how-to-print-odd-and-even-numbers-in-javascript#:~:text=const%20isOdd%20%3D%20(n)%20%3D,Even%20numbers%20are%20%24%7B%20num.
+
+
+// 2) 
---
+++ script2.js
@@ -0,0 +1,6 @@
+let start = 50
+let limit = 500
+
+for(let i = start;i <= limit;i){
+    console.log(i)
+}
