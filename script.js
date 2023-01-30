a8fb73f5a1ae2e01a895cb718a7bf81a753424d1
kelly-11 <kellyguzman2010@gmail.com>
2023-01-29 23:35:05 -0800

    lab hw

---
+++ script.js
@@ -0,0 +1,76 @@
+// SECTION 2
+
+// const a = 42.78
+// const b = 1.1
+// const c = -2
+// const d = .5
+// const e = 16
+
+// Math.floor(a)
+// console.log(Math.floor(a))
+// // 42
+// Math.ceil(b)
+// console.log(Math.ceil(b))
+// // 2
+// console.log(b - a)
+// // -41.68
+// Math.max(a, b, c, d, e)
+// console.log(Math.max(a, b, c, d, e))
+// //42.78
+// Math.min(a, b, c, d, e)
+// console.log(Math.min(a, b, c, d, e))
+// //-2
+// Math.sqrt(e)
+// console.log(Math.sqrt(e))
+// // 4
+// Math.pow(e, d)
+// console.log(Math.pow(e, d))
+// // 4
+
+// const f = Math.random() * (1, 6)
+// console.log(f)
+// random number 4.663418464192986
+
+// SECTION 3
+// Strings - Switcharoo
+// var firstVariable = ("Hello World")
+// // console.log(firstVariable);
+// // Hello World
+// myVar ();
+// function myVar() {
+//     firstVariable = 30;
+// }
+// console.log(firstVariable)
+// // 30
+// let secondVariable = firstVariable
+// console.log(secondVariable)
+// // 30
+// myFunction ();
+// function myFunction() {
+//     secondVariable = "Hello World"
+//     console.log(secondVariable)
+// }
+// // Hello World
+// console.log(firstVariable)
+// 30
+
+// Strings - Combine it
+// var yourName = "Kelly Guzman"
+// console.log(yourName)
+
+// console.log(`Hello, my name is ${yourName}`)
+
+// Section 6. Driver's Ed
+
+let age = 15
+if (age <= 16) {
+    console.log("Here are the keys!")
+} else {
+    console.log("Sorry, you're too young.")
+}
+
+
+
+
+
+
